b88bc9e9e7c7be98ab46f17abd6867e505f40f54
Rinad Hazaea <rinadhazaea@email.com>
2025-10-02 15:19:52 +0200

    Add lab-battle-of-dices.py

---
+++ lab-battle-of-dices.py
@@ -0,0 +1,35 @@
+import random
+
+Player1_wins= 0
+Player2_wins= 0
+
+player1_roll = random.randint(1,6)
+player2_roll=random.randint(1,6)
+
+print("Player 1 rolled", player1_roll)
+print("Player 2 rolled", player2_roll)
+
+roundWinner = ""
+
+if player1_roll > player2_roll:
+    roundWinner = "Player 1"
+    Player1_wins +=1
+elif player2_roll > player1_roll:
+    roundWinner = "Player 2"
+    Player2_wins +=1
+
+      
+else:
+    roundWinner = "tie"
+    
+if roundWinner == "tie":
+    print("looks like we got a tie")
+else: 
+    print(f"{roundWinner} won this round!!")
+    
+print("Result")
+print("player 1", Player1_wins)
+print("player 2", Player2_wins)
+    
+
+    

0dd784842fe45223060a703e92f513bf483f15e0
Rinad Hazaea <rinadhazaea@email.com>
2025-10-02 15:19:52 +0200

    Add dice.py

---
+++ dice.py
@@ -0,0 +1,26 @@
+import random
+
+def roll_d4():
+    return random.randint(1, 4)
+
+
+def roll_d6():
+    return random.randint(1, 6)
+
+def roll_d8():
+    return random.randint(1, 8)
+
+def roll_d12():
+    return random.randint(1, 12)
+
+def roll_d20():
+    return random.randint(1, 20)
+
+def roll_d100():
+    return random.randint(1, 100)
+
+
+
+
+
+
